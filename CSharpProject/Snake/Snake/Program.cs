5bf75749a5afa5bed61924d8cd24ed96a8722f27
Igorianba <igorqetu@mail.ru>
2016-05-27 22:07:39 +0300

    wdgw

---
+++ CSharpProject/Snake/Snake/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(1, 3, '*');
+            p1.Draw();
+
+            Point p2 = new Point(4, 5, '#');
+            p2.Draw();
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            Console.ReadLine();
+        }
+    }
+}
